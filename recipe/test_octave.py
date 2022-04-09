a449847f7f25e1950ad2c4c4174deb3486857b12
Bo <BoPeng@users.noreply.github.com>
2022-04-08 22:43:04 -0500

    Update test_octave.py

--- recipe/test_octave.py
+++ recipe/test_octave.py
@@ -3,20 +3,22 @@ import sys
 import shutil
 
 from sos_notebook.test_utils import sos_kernel
-from ipykernel.tests.utils import wait_for_idle, assemble_output
+from ipykernel.tests.utils import execute, wait_for_idle, assemble_output
 
 @unittest.skipIf(shutil.which('octave') is None, 'skip test if octave is not available')
 class TestSoSKernel(unittest.TestCase):
     def testKernel(self):
         with sos_kernel() as kc:
-            kc.execute(code='a = 1')
+            execute(kc=kc, code='a = 1')
             stdout, stderr = assemble_output(kc.get_iopub_msg)
             self.assertEqual(stdout.strip(), '', f'Stdout is not empty, "{stdout}" received')
             self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
-            kc.execute(code='%use Octave\n%get a\na')
+            execute(kc=kc, code='%use Octave\n%get a')
             stdout, stderr = assemble_output(kc.get_iopub_msg)
             self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
-            self.assertEqual(stdout.strip(), 'a =  1', f'Stdout should be a =  1, "{stdout}" received')
+            execute(kc=kc, code='a')
+            stdout, stderr = assemble_output(kc.get_iopub_msg)
+            self.assertTrue('a = 1' in stdout, f'"{stdout}" received')
 
 if __name__ == '__main__':
     unittest.main()
